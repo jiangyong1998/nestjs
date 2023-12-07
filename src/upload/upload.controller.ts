1a15dec1d337ff22bd478b7afa17f220bc3f30c4
姜勇 <jy2067@hanweb.com>
2023-12-07 17:59:22 +0800

    feat: 下载图片

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,11 +1,16 @@
 import {
   Controller,
+  Get,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { join } from 'path';
+import { zip } from 'compressing';
 
 @Controller('upload')
 export class UploadController {
@@ -15,5 +20,21 @@ export class UploadController {
   @UseInterceptors(FileInterceptor('file'))
   handleUpload(@UploadedFile() file: Express.Multer.File) {
     return file;
+  }
+
+  @Get('download')
+  handleDownload(@Res() res: Response) {
+    const url = join(__dirname, '../files/1701932315348.png');
+    res.download(url);
+  }
+
+  @Get('download-stream')
+  handleDownloadStream(@Res() res: Response) {
+    const url = join(__dirname, '../files/1701932315348.png');
+    const stream = new zip.Stream();
+    stream.addEntry(url);
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment; filename=file`);
+    stream.pipe(res);
   }
 }
