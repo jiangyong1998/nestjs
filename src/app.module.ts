51cc5b47c08c136603a71fbe78fa046316ec0264
姜勇 <jy2067@hanweb.com>
2023-12-20 16:34:24 +0800

    feat: 连接数据库、实体

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CommonModule } from './common/common.module';
 import { ConfigModule } from './config/config.module';
 import { UploadModule } from './upload/upload.module';
 import { LoginModule } from './login/login.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,19 @@ import { LoginModule } from './login/login.module';
     ConfigModule.forRoot({}),
     UploadModule,
     LoginModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: 'hanweb123',
+      host: 'localhost',
+      port: 3306,
+      database: 'demo',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], // 实体文件
+      synchronize: true, // synchronize字段代表是否自动将实体类同步到数据库
+      retryDelay: 500, // 重试连接数据库间隔
+      retryAttempts: 10, // 重试连接数据库的次数
+      autoLoadEntities: true, // 如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/login/entities/login.entity.ts
+++ src/login/entities/login.entity.ts
@@ -1,1 +1,40 @@
-export class Login {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Login {
+  // 自增列
+  @PrimaryGeneratedColumn()
+  id: number;
+  // 普通列
+  @Column({ type: 'varchar', length: 10 })
+  name: string;
+
+  @Column({ type: 'varchar', select: true }) // 定义在进行查询时是否默认隐藏此列。 设置为false时，列数据不会显示标准查询
+  password: string;
+
+  @Column({ type: 'int', nullable: true, comment: '年龄' })
+  age: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  create_time: Date;
+
+  @Generated('uuid')
+  iid: string;
+
+  @Column({ type: 'enum', enum: ['male', 'female'], default: 'male' })
+  sex: string;
+
+  // 它可以将原始数组值存储在单个字符串列中。 所有值都以逗号分隔
+  @Column('simple-array')
+  names: string[];
+
+  // 它可以存储任何可以通过 JSON.stringify 存储在数据库中的值。 当你的数据库中没有 json 类型而你又想存储和加载对象，该类型就很有用了
+  @Column('simple-json')
+  profile: { name: string; nickname: string };
+}
